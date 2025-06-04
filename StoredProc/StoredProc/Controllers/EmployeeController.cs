fa0e868577dbdf117ebd2d40e7cd5546d53d857d
MSI\ingva <inde999@gmail.com>
2025-06-04 11:24:17 +0300

    searchEmployee

---
+++ StoredProc/StoredProc/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProc.Data;
+using StoredProc.Models;
+
+namespace StoredProc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public StoredProcDbContext _context;
+        public IConfiguration _confiq { get; }
+
+        public EmployeeController
+            (
+                StoredProcDbContext context,
+                IConfiguration confiq
+            )
+        {
+            _context = context;
+            _confiq = confiq;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //teha sp andmebaasi, mis annab andmed t;;tajatest
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
+        }
+    }
+}
